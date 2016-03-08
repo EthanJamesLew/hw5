cc6d8e325bf709963cb6578173d3605fb695c030
EthanJamesLew <ethan.lew@pcc.edu>
2016-03-07 17:54:47 -0800

    Update counter.h

--- counter.h
+++ counter.h
@@ -8,20 +8,8 @@
 class Counter
 {
 public:
-	//Construct with a filepath
-	Counter(std::string fileName, std::ios_base::open_mode);
-
-	//Construct with a string
-	Counter(std::string words);
-
-	//Construct with cin
 	Counter();
-
-	void printWords();
-
-	std::string getMostUsedWord();
-
-	wordBlock getWordBlock(std::string word);
+	void printCount();
 
 private:
 	unsigned long int byteSize;
